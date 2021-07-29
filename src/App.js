052ec5aa26f438efdb0d57d0379bcd67241087c4
YirimS <yirimseck@gmail.com>
2021-07-28 23:35:11 -0700

    form in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import './App.css';
+import Main from './Main'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class App extends React.Component {
-  render(){
-    return(
-      <>
-      <h1>proof of life</h1>
-      </>
-    )
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 0,
+      long: 0,
+      showCity: true,
+      city: '',
+
+
+
+    }
   }
-}
-export default App;
+
+  handleChange = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+
+
+    render() {
+
+      return (
+        <>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Label>City Explorer</Form.Label>
+            <Form.Control onChange={this.getCity} type="text" as="input" />
+
+            <Button type="submit">Explore</Button>
+          </Form>
+
+          {/* <Main /> */}
+        </>
+      )
+    }
+  }
+} 
+  export default App;
---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Axios from 'axios'
+class Main extends React.Component {
+
+
+  render() {
+
+
+    return (
+      <h2>This is My City</h2>
+    )
+  }
+}
+export default Main;
+
+
+
+
+
