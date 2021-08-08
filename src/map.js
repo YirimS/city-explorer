bc9f8178c1007a5f7d9d069d14c75801c21b0af9
YirimS <yirimseck@gmail.com>
2021-08-07 22:28:55 -0700

    attemp to clear checks that failed

--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card'
-import Map from '/map.js'
+// import Map from '/map.js'
 
 
 class Map extends React.Component{
