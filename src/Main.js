abd54ade07c6bc32585a41caa33e93e93b3f1975
YirimS <yirimseck@gmail.com>
2021-08-07 22:23:20 -0700

    refactored Map and creates it's own component, redered error response

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
+import  
 // import Axios from 'axios'
 class Main extends React.Component {
 
 
   render() {
 
-
     return (
+      <>
       <h2>This is My City</h2>
+      <h3>
+       {this.props.lat} {this.props.lon}
+       <Card>
+         <Card.Body>you rock</Card.Body>
+      </Card>
+      </h3>
+      </>
     )
   }
 }
